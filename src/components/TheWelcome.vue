ec8a848068fa450eaee564dae1e0b27367a57831
byounghwa-min <dpatod1418@naver.com>
2025-03-05 02:29:50 +0900

    업데이트된 Vue 코드

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -25,24 +25,7 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
     </template>
     <template #heading>가방도 좀 사고!</template>
 
-    This project is served and bundled with
-    <a href="https://vite.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://vitest.dev/" target="_blank" rel="noopener">Vitest</a>
-    and
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a>
-    /
-    <a href="https://playwright.dev/" target="_blank" rel="noopener">Playwright</a>.
-
-    <br />
-
-    More instructions are available in
-    <a href="javascript:void(0)" @click="openReadmeInEditor"><code>README.md</code></a
-    >.
+    ㅉㄺ는 가방에 넣어도 눈이 아프다
   </WelcomeItem>
 
   <WelcomeItem>
