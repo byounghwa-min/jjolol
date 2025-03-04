adb20426ac15537028cd56a4c01487dd47f8acd3
byounghwa-min <dpatod1418@naver.com>
2025-03-05 01:24:23 +0900

    최초 커밋

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+<div>
+    <h2>홈페이지에 오신 것을 환영합니다!</h2>
+    <p>여기서 Vue로 만든 사이트를 개발할 수 있어요.</p>
+  </div>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
